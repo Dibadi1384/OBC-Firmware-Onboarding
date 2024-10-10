84bcd2b962547b6320373026d9fd063f2a43d013
Dibadi1384 <133037571+Dibadi1384@users.noreply.github.com>
2024-10-10 15:55:12 -0400

    Update thermal_mgr.h

--- services/thermal_mgr/thermal_mgr.h
+++ services/thermal_mgr/thermal_mgr.h
@@ -3,28 +3,43 @@
 #include "lm75bd.h"
 #include "errors.h"
 
+
+// Thermal Manager Event Types
 typedef enum {
   THERMAL_MGR_EVENT_MEASURE_TEMP_CMD,
-  
+  THERMAL_MGR_EVENT_OVER_TEMP_DETECTED,  // Added missing event
+  THERMAL_MGR_EVENT_SAFE_CONDITIONS      // Added missing event
 } thermal_mgr_event_type_t;
 
+// Thermal Manager Event Structure
 typedef struct {
   thermal_mgr_event_type_t type;
 } thermal_mgr_event_t;
+
+// Thermal Manager Configuration Structure
+typedef struct {
+  lm75bd_config_t sensorConfig;
+} thermal_mgr_config_t;
+
+// Constants for temperature thresholds
+#define THERMAL_OVER_TEMP_THRESHOLD_CELSIUS 75.0  // Example threshold
+#define THERMAL_HYSTERESIS_THRESHOLD_CELSIUS 70.0 // Example threshold
+
+// External declaration of thermal manager queue and task handles
+extern QueueHandle_t thermalMgrQueueHandle;
+extern TaskHandle_t thermalMgrTaskHandle;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+// Function declarations
 void initThermalSystemManager(lm75bd_config_t *config);
-
 error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event);
-
 void addTemperatureTelemetry(float tempC);
-
 void overTemperatureDetected(void);
-
 void safeOperatingConditions(void);
+void osHandlerLM75BD(void); // New function added
 
 #ifdef __cplusplus
 }
